d506258d5dc3845445e1b817bce3076a0e14896b
Bibesh Manandhar <bibesh@Bibeshs-MacBook-Pro.local>
2019-02-08 19:57:10 +0545

    first Update

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { GraphQLServer } from 'graphql-yoga'
+import db from './db'
+import Query from './resolvers/Query';
+import User from './resolvers/User';
+import Post from './resolvers/Post';
+import Mutation from './resolvers/Mutation';
+import Comment from './resolvers/Comment';
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers: {
+        Query,
+        User,
+        Post,
+        Comment,
+        Mutation
+    },
+    context: {
+        db
+    }
+})
+
+server.start(() => {
+    console.log('The server is up!')
+})
